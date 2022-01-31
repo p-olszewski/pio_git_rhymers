7b80cd2a9b80c121661f8c50999d4376ccf9eb14
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-01-31 20:02:31 +0100

    1.4 stale 1 Zamiana literalow na stale

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int ARRAY_SIZE = 12;
+    public static final int INDEX_OF_EMPTY_ARRAY = -1;
+    public static final int INDEX_OF_FULL_ARRAY = 11;
+    private int[] numbers = new int[ARRAY_SIZE];
 
-    public int total = -1;
+    public int total = INDEX_OF_EMPTY_ARRAY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INDEX_OF_EMPTY_ARRAY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == INDEX_OF_FULL_ARRAY;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return INDEX_OF_EMPTY_ARRAY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return INDEX_OF_EMPTY_ARRAY;
         return numbers[total--];
     }
 
